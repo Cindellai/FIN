892cafcd0338a8d8d3710131e887c97759871575
cindellai <cindellai26@gmail.com>
2024-08-02 16:12:58 -0500

    "Added authentication and CRUD functionality for subscriptions and trades, updated routes and stylesheets"

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    if @user.trader?
+      @posts = @user.posts
+      @trades = @user.trades
+    end
+    @subscriptions = @user.subscriptions if current_user.student?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+   # Define a route for the user profile page
+   get 'users/:id', to: 'users#show', as: 'user_profile'
   
   resources :subscriptions
   resources :trades
