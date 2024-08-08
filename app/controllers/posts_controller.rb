193fa5e29595252e63cca47693f605c4fc439585
cindellai <cindellai26@gmail.com>
2024-08-08 12:24:47 -0500

    added validations, alerts, active storage video functionality for new posts form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
       redirect_to @post, notice: 'Post was successfully created.'
     else
       @form_submit_text = "Create Post"
+      flash.now[:alert] = @post.errors.full_messages.join(', ')
       render :new
     end
   end
@@ -31,6 +32,7 @@ class PostsController < ApplicationController
       redirect_to @post, notice: 'Post was successfully updated.'
     else
       @form_submit_text = "Update Post"
+      flash.now[:alert] = @post.errors.full_messages.join(', ')
       render :edit
     end
   end
@@ -50,6 +52,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :body, :post_type, :url, :file, trade_attributes: [:id, :stock_name, :executed_at, :performance, :buy_or_sell, :quantity, :price, :description, :_destroy, :poster_id])
+    params.require(:post).permit(:title, :body, :post_type, :file, trade_attributes: [:id, :stock_name, :executed_at, :performance, :buy_or_sell, :quantity, :price, :description, :_destroy, :poster_id])
   end
 end
