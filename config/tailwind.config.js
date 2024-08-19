1650582d46273bdd8d97e964409b126d2eeabe8d
cindellai <cindellai26@gmail.com>
2024-08-18 22:12:43 -0500

    edited home page and did the news

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -58,6 +58,41 @@ module.exports = {
       }
     }
   },
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const scrollElements = document.querySelectorAll('.scroll-animate');
+
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <= 
+            (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add('scroll-animate-visible');
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove('scroll-animate-visible');
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            } else {
+                hideScrollElement(el);
+            }
+        })
+    }
+
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+    });
+}),
+
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
